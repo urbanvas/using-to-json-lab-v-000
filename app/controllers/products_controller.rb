4d1d633db10fd6c67377a8d82243a136f4e90fce
Neil Seejoor <nvseejoor@gmail.com>
2019-04-09 19:53:09 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,6 +24,10 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @product.to_json(only: [:id, :name, :description, :inventory, :price])}
+    end
   end
 
   def data
